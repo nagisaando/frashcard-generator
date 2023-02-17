eed266c18847151e1992f4f2fcab53a921f63bc1
Nagisa Ando <nagisa@hanayoku.com>
2023-02-16 18:15:10 -0800

    refactor code

--- src/App.svelte
+++ src/App.svelte
@@ -1,42 +1,5 @@
 <script lang="ts">
   import ClassList from './lib/ClassList.svelte'
-  import Button, {Label} from '@smui/button'
-  let classSpan: Array<Node> = []
-  let messages: Array<String> = []
-  let messagesText: string
-
-  async function convertHtmlClassToMessageStrArray() {
-    const response = await fetch('/src/class/class-1.html')
-    let htmlString = await response.text()
-
-    const parser = new DOMParser()
-    const htmlDoc = parser.parseFromString(htmlString, 'text/html')
-    classSpan = [...htmlDoc.querySelectorAll("[data-qa-id='message-text']")]
-
-    classSpan.forEach((span) => {
-      if (span instanceof HTMLElement) {
-        messages.push(span.innerText)
-      }
-    })
-    messagesText = messages.join('\r\n')
-  }
-  convertHtmlClassToMessageStrArray()
-
-  /** Download contents as a file
-   * Source: https://stackoverflow.com/questions/14964035/how-to-export-javascript-array-info-to-csv-on-client-side
-   */
-  function downloadBlob(content: string, filename: string, contentType: string): void {
-    // Create a blob
-    var blob = new Blob([content], {type: contentType})
-    var url = URL.createObjectURL(blob)
-
-    // Create a link to download it
-    var pom = document.createElement('a')
-    pom.href = url
-    pom.setAttribute('download', filename)
-    pom.click()
-  }
-  // create csv download button
 </script>
 
 <svelte:head>
@@ -52,12 +15,6 @@
 <main>
   <h1>Frashcard generator</h1>
   <ClassList />
-  <Button
-    on:click={() => downloadBlob(messagesText, 'export.csv', 'text/csv;charset=utf-8;')}
-    variant="raised"
-  >
-    <Label>produce csv file</Label>
-  </Button>
 </main>
 
 <style>
--- src/lib/ClassList.svelte
+++ src/lib/ClassList.svelte
@@ -1,23 +1,17 @@
 <script lang="ts">
   import DataTable, {Head, Body, Row, Cell} from '@smui/data-table'
-  import LinearProgress from '@smui/linear-progress'
-  import Button, {Label} from '@smui/button'
   import DownloadButton from './DownloadButton.svelte'
   import {onMount} from 'svelte'
-  type User = {
-    date: number
-    name: string
-    username: string
-    email: string
-    website: string
-  }
-  let items: User[] = []
+
   let loaded = false
-
   let classList: Array<ClassData> = []
-  const requests: Array<Promise<Document>> = []
 
-  // create interface for this data
+  interface ClassData {
+    messages: Array<HTMLElement>
+    date: String | undefined
+    tutor: String | undefined
+  }
+
   interface ResponseData {
     text(): Promise<string>
     body: ReadableStream<Uint8Array> | null
@@ -30,71 +24,44 @@
     type: String
     url: String
   }
-  interface ClassData {
-    messages: Array<HTMLElement>
-    date: String | undefined
-    tutor: String | undefined
-  }
-  async function convertDataToHTML(data: ResponseData) {
-    let htmlString = await data.text()
-    const parser = new DOMParser()
-    return parser.parseFromString(htmlString, 'text/html')
-  }
+
   function getClassData(documents: Array<Document>) {
     documents.forEach((document: Document, index: number, array: Document[]) => {
       let classData: ClassData = {messages: [], date: '', tutor: ''}
       classData.messages = [...document.querySelectorAll("[data-qa-id='message-text']")].map(
         (el) => el as HTMLElement
       )
-      classData.tutor = document.querySelector("[data-qa-id='collocutor_name']")?.innerText
+      classData.tutor = document.querySelector<HTMLElement>(
+        "[data-qa-id='collocutor_name']"
+      )?.innerText
       classData.date = document.querySelector<HTMLElement>("[class^='DateSeparator']")?.innerText
       classList.push(classData)
     })
     classList = classList
   }
-  for (let i = 1; i <= 24; i++) {
-    async function convertHTML() {
-      const response = await fetch(`/src/class/class-${i}.html`)
-      const htmlString = await convertDataToHTML(response) // add more function here also fix response data
-      return htmlString
+  async function convertDataToHTML(data: ResponseData) {
+    let htmlString = await data.text()
+    const parser = new DOMParser()
+    return parser.parseFromString(htmlString, 'text/html')
+  }
+  async function getHTMLData() {
+    const requests: Array<Promise<Document>> = []
+    for (let i = 1; i <= 24; i++) {
+      async function convertHTML() {
+        const response = await fetch(`/src/class/class-${i}.html`)
+        const htmlString = await convertDataToHTML(response) // add more function here also fix response data
+        return htmlString
+      }
+      requests.push(convertHTML())
     }
-    requests.push(convertHTML())
+    const responses = await Promise.all(requests)
+    return responses
   }
-  async function convertHtmlClassToMessageStrArray() {
-    const htmlDocuments = await Promise.all(requests)
-    getClassData(htmlDocuments)
-  }
-
-  onMount(() => {
-    convertHtmlClassToMessageStrArray()
-    loadThings(false)
+  onMount(async () => {
+    const responses = await getHTMLData()
+    getClassData(responses)
   })
-
-  function loadThings(wait: boolean) {
-    if (typeof fetch !== 'undefined') {
-      loaded = false
-
-      fetch(
-        'https://gist.githubusercontent.com/hperrin/e24a4ebd9afdf2a8c283338ae5160a62/raw/dcbf8e6382db49b0dcab70b22f56b1cc444f26d4/users.json'
-      )
-        .then((response) => response.json())
-        .then((json) =>
-          setTimeout(
-            () => {
-              items = json
-              loaded = true
-            },
-            // Simulate a long load time.
-            wait ? 2000 : 0
-          )
-        )
-    }
-  }
 </script>
-
-<div style="margin-bottom: 1em;">
-  <Button on:click={() => loadThings(true)}>Do Pretend Loading</Button>
-</div>
 
 <DataTable table$aria-label="User list" style="width: 100%;">
   <Head>
@@ -113,11 +80,4 @@
       </Row>
     {/each}
   </Body>
-
-  <LinearProgress
-    indeterminate
-    bind:closed={loaded}
-    aria-label="Data is being loaded..."
-    slot="progress"
-  />
 </DataTable>
