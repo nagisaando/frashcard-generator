a25b543496efeffd9918c8c488291548a01892fc
Nagisa Ando <amnot.nobig.deal@gmail.com>
2023-02-27 18:28:10 -0800

    add openai

--- server/index.js
+++ server/index.js
@@ -1,25 +1,55 @@
 const express = require('express')
+require('dotenv').config();
+const { Configuration, OpenAIApi } = require("openai");
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
 
 const app = express()
+app.use(express.json())
 
+app.post('translate-to-english', (req, res) => {
 
-app.get("/greeting", (req, res) => {
-    const { name } = req.query
-    if (name.toLowerCase() == 'daniel' || name.toLocaleLowerCase().includes('daniel')) {
-        res.send('こんにちは😃　ダニエル！')
-    } else if (name) {
-        res.send(`Hey ${name}.... but where is Daniel?`)
-    } else {
-        res.send('You better to say your name first!')
+    const animal = req.body.animal || '';
+    if (animal.trim().length === 0) {
+        res.status(400).json({
+            error: {
+                message: "Please enter a valid animal",
+            }
+        });
+        return;
     }
 
+    try {
+        const completion = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: generatePrompt(animal),
+            temperature: 0.6,
+        });
+        res.status(200).json({ result: completion.data.choices[0].text });
+    } catch (error) {
+        // Consider adjusting the error handling logic for your use case
+        if (error.response) {
+            console.error(error.response.status, error.response.data);
+            res.status(error.response.status).json(error.response.data);
+        } else {
+            console.error(`Error with OpenAI API request: ${error.message}`);
+            res.status(500).json({
+                error: {
+                    message: 'An error occurred during your request.',
+                }
+            });
+        }
+    }
 })
 
 
-app.get("/api", (req, res) => {
-    res.json({ "users": ["user 1", "user 2"] })
-})
 
-app.listen(3000, () => {
+const port = process.env.PORT || 5000
+
+app.listen(port, () => {
     console.log('server started')
 })
+
