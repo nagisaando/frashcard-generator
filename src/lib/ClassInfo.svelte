63875f3d3e8cb01a1ac6ce8cdd69ffd726adc307
Nagisa Ando <amnot.nobig.deal@gmail.com>
2023-03-05 10:42:15 -0800

    add funciton

--- src/lib/ClassInfo.svelte
+++ src/lib/ClassInfo.svelte
@@ -6,29 +6,58 @@
   export let tutor: String | undefined
   export let date: String | undefined
 
+  async function detectLanguage(text: string) {
+    return await fetch(`http://localhost:3000/detect-language`, {
+      method: 'POST',
+      body: JSON.stringify({text}), // Replace with your data object
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((response) => response.text()) // convert the response to text format
+      .then((data) => {
+        return data
+      })
+    // check how to send a response
+  }
+  function getRevisedMessges() {
+    // this has to be modified
+    const requests: Array<Promise<>> = []
+    messages.forEach((message) => {
+      async function translateMessage() {
+        const lang = await detectLanguage(message)
+      }
+      requests.push(translateMessage())
+    })
+
+    const requests: Array<Promise<Document>> = []
+    for (let i = firstClassIndex; i <= classFileNumber; i++) {
+      async function convertHTML() {
+        const response = await fetch(`/src/class/class-${i}.html`)
+        const htmlString = await convertDataToHTML(response) // add more function here also fix response data
+        return htmlString
+      }
+      requests.push(convertHTML())
+    }
+  }
   onMount(() => console.log('rendered'))
 </script>
 
+<div>
+  <dir>
+    <h2>Class Info</h2>
+    <ul>
+      <li>Tutor: {tutor}</li>
+      <li>Date: {date}</li>
+    </ul>
+  </dir>
+  <!-- create button to analise -->
+  <Button on:click={() => getRevisedMessges()}>
+    <Label>Analysis the messages</Label>
+  </Button>
+</div>
 <DataTable table$aria-label="class info" style="width: 100%; margin-top: 3rem;">
   <Head>
-    <Row>
-      <Cell class="table-header">
-        <dir>
-          <h2>Class Info</h2>
-          <ul>
-            <li>Tutor: {tutor}</li>
-            <li>Date: {date}</li>
-          </ul>
-        </dir>
-        <!-- create button to analise -->
-        <Button>
-          <Label>Analysis the messages</Label>
-        </Button>
-      </Cell>
-      {#each Array(3) as i}
-        <Cell style="width: 0; padding: 0" />
-      {/each}
-    </Row>
     <Row>
       <Cell>Message</Cell>
       <Cell>Lang</Cell>
